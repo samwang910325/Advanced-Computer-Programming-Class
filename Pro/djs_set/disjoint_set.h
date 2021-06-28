54d095ca1ff065138b76fdc4a90479129f9add54
samwang910325 <73706005+samwang910325@users.noreply.github.com>
2021-06-28 23:29:38 +0800

    Update disjoint_set.h

--- Pro/djs_set/disjoint_set.h
+++ Pro/djs_set/disjoint_set.h
@@ -6,10 +6,10 @@ typedef struct disjoint_set
     
 }disjoint_set;
 
-disjoint_set* djs_gen(int range);
+disjoint_set* djs_gen(void);
 
-disjoint_set* djs_make_set(disjoint_set* q);
+disjoint_set* djs_make_set(int v,disjoint_set* q);
 
 disjoint_set* djs_find_set(disjoint_set* q);
 
-int djs_Union(disjoint_set* a,disjoint_set *b);   
+int djs_Union(disjoint_set* a,disjoint_set *b) ;
